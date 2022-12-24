de7540356736b66bfb23b35a9598095c840b5c81
Nitin Parihar <109915933+Nitin-parihar09@users.noreply.github.com>
2022-12-24 12:14:49 +0530

    Add files via upload

---
+++ StudentManagement/src/main/java/com/nitin/Service/StudentService.java
@@ -0,0 +1,10 @@
+package com.nitin.Service;
+
+import java.util.List;
+
+import com.nitin.Entity.Students;
+
+public interface StudentService {
+	List<Students> getAllStudents();
+	Students saveStudent(Students student);
+}
